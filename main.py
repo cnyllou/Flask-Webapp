33c20d63da46a23c0083ecd0dd229d05ef6a605b
unknown <cnyllou@gmail.com>
2020-10-01 22:42:10 +0300

    Sākt izstrādāt mājaslapu

--- main.py
+++ main.py
@@ -1,122 +1,13 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
-from admin.second import second
 
 app = Flask(__name__)
-app.register_blueprint(second, url_prefix="/admin")
+app.secret_key = "oashdaiudh8921ue19dh93820dusdnamm0a28a"
 
-app.secret_key = "hello"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3' # users - table name
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.permanent_session_lifetime = timedelta(days=5)
-
-db = SQLAlchemy(app)
-
-class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
 
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/view", methods=["POST", "GET"])
-def view():
-    if request.method == "POST":
-        delete_user = request.form["delete_user"]
-        add_username = request.form["add_username"]
-        add_email = request.form["add_email"]
-
-        found_username = users.query.filter_by(name=add_username).first() 
-        found_email = users.query.filter_by(name=add_email).first() 
-        if found_username or found_email:
-            flash("Username [ {add_username} ] or [ {add_email} ] already in database")
-        else:
-            usr = users(add_username, add_email)
-            db.session.add(usr)
-            db.session.commit()
-
-        try:
-            if delete_user != "":
-                found_user_delete = users.query.filter_by(name=delete_user).first()
-                db.session.delete(found_user_delete)
-                db.session.commit() 
-                flash(f"User [ {delete_user} ] was deleted!")
-                #return redirect(url_for('view'))
-        except:
-            flash(f"User [ {delete_user} ] not in database!")
-        
-
-    return render_template("view.html", values=users.query.all())
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login_page():
-    if request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        session["user"] = user
-
-        found_user = users.query.filter_by(name=user).first() # Atrod lietotājus ar ierakstīto vārdu
-        if found_user:
-            session["email"] = found_user.email
-        else:
-            usr = users(user, "")
-            db.session.add(usr)
-            db.session.commit() # Pie visām datu bāzes izmaiņām jāraksta apstiprinājums
-
-        flash("Login successful!")
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            return redirect(url_for("user"))
-            
-        return render_template("login_form.html")
-
-
-@app.route("/user", methods=["POST", "GET"])
-def user():
-    email = None
-    if "user" in session:
-        user = session["user"]
-        
-        if request.method == "POST":
-            email = request.form["email"]
-            session["email"] = email
-            
-            found_user = users.query.filter_by(name=user).first()
-            found_user.email = email
-            db.session.commit() 
-            flash("Email was saved!")
-        else:
-            if "email" in session:
-                email = session["email"]
-
-        return render_template("user_profile.html", email=email)
-    else:
-        flash("You are not logged in!")
-        return render_template("login_form.html")
-
-
-@app.route("/logout")
-def logout():
-    if "user" in session:
-        flash("You have been logged out!", "info")
-        session.pop("user", None)
-        session.pop("email" , None)
-    else:
-        flash("You are already logged out!", "info")
-
-    return redirect(url_for("login_page"))
-
-
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True)
---
+++ notes/Darāmais.md
@@ -0,0 +1,67 @@
+---
+title: Darāmais
+created: '2020-10-01T18:50:39.295Z'
+modified: '2020-10-01T19:33:59.167Z'
+---
+
+# Darāmais
+## Priekšlapa
+- [ ] Navigāciju augšā ar logout labajā pusē
+- [ ] Kreisajā pusē galveno navigāciju ar izvēlēm
+  - Virs visa ir lietotājs ar
+    - profila bildi
+    - Vārdu, Uzvārdu
+    - Pozīciju
+  - Viss inventārs
+  - Pieprasīt inventāru
+- [ ] Ielogošanās lapa (Vienkārša paskata lapa)
+  - E-pasts
+  - Parole
+- [ ] Galvenē var apskatīt visu inventāru
+  - [ ] Filtru sadaļa
+  - [ ] Meklēšanas lauks
+  - [ ] Visi vienumi apskatāmi
+    - Bildes (Ja nav tad, noklusējuma bilde)
+    - Opcijas
+      - Apskatīt
+      - Rediģēt
+    - Informācija
+      - Nosaukums
+      - Operētājsistēma
+      - Atrašanās vieta
+      - Kam piesaistīts
+      - ID (XXXX)
+- [ ] Administratora profils
+  - Manto lietotāja izvēlni
+  - Lietotāju pārvalde
+- [ ] Lietotāju pārvalde
+  - Administrators tikai var piekļūt
+  - Iespējams:
+    - Piereģistrēt, dzēst, modificēt lietotājus
+    - Pievienot, dzēst, modificēt ierakstus inventāram
+
+## Datubāzes daļa (Mini datubāze priekš mājaslapas būvēšanas)
+- [ ] - Lietotāji
+  - ID, Vārds, Uzvārds, E-pasts, Parole, Pozīcija 
+- [ ] - Inventārs
+  - ID, Kategorija, Ražotājs, Nosaukums, Modelis, 
+  - bilde, Apraksts,
+  - atrašanās vieta,  piesaistītais
+
+## Prasības
+- [ ] Reģistrācija notiek no administratora puses
+  - Lietotājs ar administratora privilēģijām veido jaunu ierakstu datubāzē
+- [ ] Atverot mājaslapu parādas ielogošanās lapa
+  - Vienkārša lapa ar tikai e-pastu un paroli
+- [ ] Mājas lapā lietotājs var
+  - Apskatīt visu invetāru
+  - Apskatīt savu piesaistīto invetāru
+  - Var filtrēt inventāru
+  - Pievienot jaunu inventāru
+  - Virspusēji redzēt savu informāciju
+  - Izlogoties
+- [ ] Administratora profils
+  - Manto visu to, ko lietotājs var izdarīt
+  - Var rediģēt, pievienot, dzēst darbiniekus no datubāzes
+- [ ] Pievienot jaunu tehniku lapa
+  - Jebkurš lietotājs var pievienot jaunu vienumu esošajam inventāram
