e2f4b46d643025ef0745787e030db9505c2c57a7
unknown <cnyllou@gmail.com>
2020-10-03 07:14:23 +0300

    Sākt ielogošanās lapu

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+@app.route("/login")
+def login():
+    return render_template("auth/login.html")
+
 @app.route("/home")
 @app.route("/")
 def home():
